a5396c579120b728de219d00fc8ddf8b92bc86f0
杨伦 <yanglun3@asiainfo.com>
2019-07-06 00:10:32 +0800

    spi思想学习

--- src/main/java/com/yl/base/spi/Person.java
+++ src/main/java/com/yl/base/spi/Person.java
@@ -1,5 +1,16 @@
 package com.yl.base.spi;
 
+/**
+ * @description SPI接口
+ *
+ * @version v1.1.0
+ * @author yanglun
+ * @date  2019/7/6 0:09
+ * Modification History:
+ *   Date           Author          Version            Description
+ *-------------------------------------------------------------
+ *    2019/7/6      yanglun            v1.0.0              修改原因
+ */
 public interface Person {
 
     void sayHello();
---
+++ src/main/java/com/yl/base/spi/TestSPI.java
@@ -0,0 +1,17 @@
+package com.yl.base.spi;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+public class TestSPI {
+
+    public static void main(String[] args) {
+        ServiceLoader<Person> loader = ServiceLoader.load(Person.class);
+        Iterator<Person> iterator = loader.iterator();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            person.sayHello();
+        }
+    }
+
+}
