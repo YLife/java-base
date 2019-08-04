402bf8878a22a0ba3e8712f0efda9205cd7457d8
Sunshine <359059275@qq.com>
2019-08-04 23:46:09 +0800

    study kafka

---
+++ src/main/java/com/yl/base/jms/kafka/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package com.yl.base.jms.kafka;
+
+import com.yl.base.jms.AbstractJMSConsumer;
+
+/**
+ * @author yanglun
+ * @description: kafka consumer test
+ * @date 2019/8/4 23:32
+ */
+public class KafkaConsumer extends AbstractJMSConsumer {
+
+    @Override
+    public void establishConnection() throws Exception {
+
+    }
+
+    @Override
+    protected void consumeMessageByP2P() throws Exception {
+
+    }
+
+    @Override
+    protected void consumeMessageByPS() throws Exception {
+
+    }
+
+}
